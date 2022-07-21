7ea06b302e2734c066f9c02834bd97c28ea0bda3
Rahulmsutar <rahulsutar959@gmail.com>
2022-07-21 18:08:14 +0530

    installation routing code

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+import React from 'react'
+import { BroswerRouter as Router, Routes, Route, useRoutes } from 'react-router-dom'
+import Home from './pages/Home';
+import Demo from './pages/Demo';
+import Keyfeature from './pages/Keyfeature';
+import Pricing from './pages/Pricing ';
+import Testmonials from './pages/Testmonials';
+import Navbar from './Navbar/Navbar';
 
 function App() {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/features",
+      element: <Keyfeature />
+    },
+    {
+      path: "/pricing",
+      element: <Pricing />
+    },
+    {
+      path: "/testimonials",
+      element: <Testmonials />
+    },
+    {
+      path: "/demo",
+      element: <Demo />
+    },
+  ])
   return (
-    <div className="App">
-
+    <div className="container">
+      <Navbar />
+      {routes}
     </div>
   );
 }
---
+++ src/components/Navbar/Menulist.js
@@ -0,0 +1,22 @@
+export const Menulist = [
+    {
+        title: "Home",
+        url: "/"
+    },
+    {
+        title: "Keyfeatures",
+        url: "/features"
+    },
+    {
+        title: "Pricing",
+        url: "/pricing"
+    },
+    {
+        title: "Testimonials",
+        url: "/testimonials"
+    },
+    {
+        title: "Try fro Free",
+        url: "/demo"
+    },
+]
